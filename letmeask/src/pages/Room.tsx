23d081d04876b782d69d2669510e776b7dbc74d4
michaelfarias <michaelfarias01@gmail.com>
2021-08-05 16:50:17 -0300

    exibiindo perguntas estruturadas

--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { Question } from '../components/Question';
 
 import logoImg from '../assets/images/logo.svg';
 
@@ -10,13 +11,12 @@ import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
 
 import '../styles/room.scss';
-import { stringify } from 'querystring';
 
 type RoomParams = {
     id: string;
 }
 
-type Question = {
+type QuestionType = {
     id: string;
     author: {
         name: string,
@@ -41,7 +41,7 @@ export function Room() {
     const { user } = useAuth();
     const params = useParams<RoomParams>();
     const [newQuestion, setNewQuestion] = useState('');
-    const [questions, setQuestions] = useState<Question[]>([]);
+    const [questions, setQuestions] = useState<QuestionType[]>([]);
     const [title, setTitle] = useState('');
 
     const roomId = params.id;
@@ -132,7 +132,20 @@ export function Room() {
                         <Button type="submit" disabled={!user}>Enviar pergunta</Button>
                     </div>
                 </form>
-                {JSON.stringify(questions)}
+
+                <div className="question-list">
+                    {
+                        questions.map(question => {
+                            return (
+                                <Question
+                                    key={question.id}
+                                    content={question.content}
+                                    author={question.author}
+                                />
+                            )
+                        })
+                    }
+                </div>
             </main>
         </div>
     );
